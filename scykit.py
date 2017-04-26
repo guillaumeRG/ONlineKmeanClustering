fixes computation time precision + add progressive mode with -s

scykit.py
@@ -24,7 +24,29 @@ def _tolerance(X, tol):
     else:
         variances = np.var(X, axis=0)
     return np.mean(variances) * tol
-
+def input_num(prompt):
+    """
+    prompt the user for a numeric input
+    prompt again if the input is not numeric
+    return an integer or a float
+    """
+    while True:
+        # strip() removes any leading or trailing whitespace
+        num_str = input(prompt).strip()
+        if num_str == 'stop':
+            return 'stop'
+        num_flag = True
+        for c in num_str:
+            # check for non-numerics
+            if c not in '+-.0123456789':
+                num_flag = False
+        if num_flag:
+            break
+    # a float contains a period (US)
+    if '.' in num_str:
+        return float(num_str)
+    else:
+        return int(num_str)
 if __name__ == '__main__':
   
   
@@ -58,8 +80,8 @@ if __name__ == '__main__':
   #to do with online MBK_mean  
   #Compute clustering with MiniBatchKMeans
   mbk = cluster.MiniBatchKMeans(init=init, n_clusters=3, batch_size=batch_size,n_init=10, max_no_improvement=n_iter, verbose=0)
-  t0 = time.time()
   
+  #INIT THREADs
   try:
    if sys.argv[2] == '-pp' or sys.argv[3] == '-pp':
     thread_1 = Afficheur('starting threads',labels_true,mbk,k_means,X,n_clusters,t_batch)
@@ -67,28 +89,118 @@ if __name__ == '__main__':
   except IndexError:
    pass
    
+  try:
+   if sys.argv[2] == '-s':
+    
+    #init state
+    n_batches = int(np.ceil(float(n_samples) / batch_size))
+    max_iter = 100
+    
    
-
+    tol=0
+    _, n_features = X.shape
+    old_center_buffer = np.zeros(n_features, dtype=X.dtype)
+    random_state = check_random_state(None)
+    init_size = 3 * batch_size
+    if init_size > n_samples:
+     init_size = n_samples
+     
+    validation_indices = random_state.randint(0, n_samples, init_size)
+    X_valid = X[validation_indices]
+    x_squared_norms = row_norms(X, squared=True)
+    x_squared_norms_valid = x_squared_norms[validation_indices]
+    counts = np.zeros(n_clusters, dtype=np.int32)
+    best_inertia = None
+    cluster_centers = None
+    
+    for init_idx in range(n_init):
+     cluster_centers = cluster._init_centroids(X, n_clusters, init, random_state=random_state, x_squared_norms=x_squared_norms, init_size=init_size)
+     batch_inertia, centers_squared_diff = cluster._mini_batch_step(X_valid, x_squared_norms[validation_indices], cluster_centers, counts, old_center_buffer, False, distances=None, verbose=False)
+     _, inertia = cluster._labels_inertia(X_valid, x_squared_norms_valid, cluster_centers)
+     if best_inertia is None or inertia < best_inertia:
+      mbk.cluster_centers_ = cluster_centers
+      mbk.counts_ = counts
+      best_inertia = inertia
+      print('best inertia %d' %best_inertia)
+       
+    while(True):
+     thread_1 = Afficheur('starting threads',labels_true,mbk,k_means,X,n_clusters,t_batch)
+     thread_1.start()
+     t0 = time.time()
+    
+    
+     for iteration_idx in range(n_iter):
+      minibatch_indices = random_state.randint(0, n_samples, batch_size)
+      mbk=mbk.partial_fit(X[minibatch_indices])
+      thread_1.update(mbk)
+     t_mini_batch = time.time() - t0
+     thread_1.stop()
+     thread_1.join()
+     n_iter = input_num("Iterations suivante : ")
+     if n_iter == "stop":
+      break
+     if isinstance(n_iter, int) == False:
+      print('error integer is required !!! type %s'%type(n_iter))
+      break
+   
+  except IndexError:
+   pass   
   try:
    if sys.argv[2] == '-pp':
     random_state = check_random_state(None) 
-    
+    t0 = time.time()
     # Sample a minibatch from the full dataset
     for iteration_idx in range(n_iter-1):
      minibatch_indices = random_state.randint(0, n_samples, batch_size)
      mbk=mbk.partial_fit(X[minibatch_indices])
      thread_1.update(mbk)
+    t_mini_batch = time.time() - t0
     thread_1.stop()
+    
   except IndexError:
    pass   
-     
-     
-     
+       
+  try:
+   if sys.argv[2] == '-p':
+    tol=0
+    random_state = check_random_state(None)
+    t0 = time.time()
+    for iteration_idx in range(n_iter):
+     minibatch_indices = random_state.randint(0, n_samples, batch_size)
+     mbk=mbk.partial_fit(X[minibatch_indices])
+     tol = _tolerance(X, tol)
+    t_mini_batch = time.time() - t0
+   
+  except IndexError:
+   pass
+    
+  try: 
+   if sys.argv[2] == '-n':
+    t0 = time.time()
+    mbk=mbk.fit(X)
+    t_mini_batch = time.time() - t0
+    
+   
+  except IndexError:
+   pass
+   
+  try :
+   if sys.argv[2] == None:
+    random_state = check_random_state(None)   
+    # Sample a minibatch from the full dataset
+    t0 = time.time()
+    for iteration_idx in range(n_iter-1):
+     minibatch_indices = random_state.randint(0, n_samples, self.batch_size) 
+     mbk=mbk.partial_fit(X,minibatch_indices=minibatch_indices) 
+    t_mini_batch = time.time() - t0  
+  except IndexError:
+   pass
 
   try:  
    if sys.argv[2] == '-o':
     n_batches = int(np.ceil(float(n_samples) / batch_size))
     max_iter = 100
+    
     n_iter = int(max_iter * n_batches)
     tol=0
     _, n_features = X.shape
@@ -126,7 +238,7 @@ if __name__ == '__main__':
      convergence_context = {}
      mbk.batch_inertia = batch_inertia
      mbk.centers_squared_diff = centers_squared_diff
-     
+     t0 = time.time()
      for iteration_idx in range(n_iter):
       minibatch_indices = random_state.randint(0, n_samples, batch_size)
       mbk=mbk.partial_fit(X[minibatch_indices])
@@ -135,12 +247,15 @@ if __name__ == '__main__':
 
       # Monitor convergence and do early stopping if necessary
       if cluster._mini_batch_convergence(mbk, iteration_idx, n_iter, tol, n_samples,mbk.centers_squared_diff, mbk.batch_inertia, convergence_context,verbose=mbk.verbose):
+       t_mini_batch = time.time() - t0
+       thread_1.stop()
        break
-     thread_1.stop()
+     
    
     elif sys.argv[3] == '-p':
      random_state = check_random_state(None)
      convergence_context = {}
+     t0 = time.time()
      for iteration_idx in range(n_iter):
       minibatch_indices = random_state.randint(0, n_samples, batch_size)
       mbk=mbk.partial_fit(X[minibatch_indices])
@@ -148,40 +263,19 @@ if __name__ == '__main__':
      
       # Monitor convergence and do early stopping if necessary
       if cluster._mini_batch_convergence(mbk, iteration_idx, n_iter, tol, n_samples,mbk.centers_squared_diff, mbk.batch_inertia, convergence_context,verbose=False):
+       t_mini_batch = time.time() - t0
+      
        break
   except IndexError:
    pass
   
-  try:
-   if sys.argv[2] == '-p':
-    tol=0
-    random_state = check_random_state(None)
-   
-    for iteration_idx in range(n_iter):
-     minibatch_indices = random_state.randint(0, n_samples, batch_size)
-     mbk=mbk.partial_fit(X[minibatch_indices])
-     tol = _tolerance(X, tol)
-  except IndexError:
-   pass
-    
-  try: 
-   if sys.argv[2] == '-n':
-    mbk=mbk.fit(X)
-  except IndexError:
-   pass
-  try :
-   if sys.argv[2] == None:
-    random_state = check_random_state(None)   
-    # Sample a minibatch from the full dataset
-     
-    for iteration_idx in range(n_iter-1):
-     minibatch_indices = random_state.randint(0, n_samples, self.batch_size) 
-     mbk=mbk.partial_fit(X,minibatch_indices=minibatch_indices)   
-  except IndexError:
-   pass
+  
+  
+  
+  #plotting
   
   try:
-   if sys.argv[2] == '-p' or sys.argv[2] == '-pp' or sys.argv[3] == '-pp' or sys.argv[3] == '-p':
+   if sys.argv[2] == '-p' or sys.argv[2] == '-pp' or sys.argv[2] == '-s' or sys.argv[3] == '-pp' or sys.argv[3] == '-p':
     print('plotting')
     #Plot result
     fig = plt.figure(figsize=(8, 3))
@@ -211,7 +305,7 @@ if __name__ == '__main__':
     plt.text(-3.5, 1.8,  'train time: %.2fs\ninertia: %f' % (
         t_batch, k_means.inertia_))
 
-    t_mini_batch = time.time() - t0
+    
     # MiniBatchKMeans
     ax = fig.add_subplot(1, 3, 2)
     for k, col in zip(range(n_clusters), colors):
@@ -264,7 +358,7 @@ if __name__ == '__main__':
     print('Difference \'s ratio: %f'%ratio)
    
     
-    if sys.argv[2] == '-p' or sys.argv[2] == '-pp' or sys.argv[3] == '-pp' or sys.argv[3] == '-p':
+    if sys.argv[2] == '-p' or sys.argv[2] == '-pp'or sys.argv[2] == '-s' or sys.argv[3] == '-pp' or sys.argv[3] == '-p' :
      plt.show()
     if (sys.argv[3] == '-f') and (sys.argv[4] != None):
      print('Saving...')
@@ -272,11 +366,7 @@ if __name__ == '__main__':
      print('Done !')
     elif sys.argv[3] == '-f' and sys.argv[4] == None:
      print('Usage : -f PATH/NAME')
-    
-   
-    
-    
-   
+
   except IndexError:
    pass 
    